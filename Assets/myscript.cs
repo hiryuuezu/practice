db7e91ecc76071e3c18ed524954f625acc3da2e9
上江洲飛龍 <hiryu2088@gmail.com>
2018-07-07 11:39:00 +0900

    second commit

--- Assets/myscript.cs
+++ Assets/myscript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +9,8 @@ public class myscript : MonoBehaviour {
 
 	void Start () {
 		Debug.Log ("unnko");
+        Debug.log("huwibsi");
+        
 
 	}
 	
