f2d28c22955e32ee60be1d02661741ef9870b8f9
上江洲飛龍 <hiryu2088@gmail.com>
2018-07-07 10:54:23 +0900

    first commit

---
+++ Assets/myscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myscript : MonoBehaviour {
+	float zvalue = 0f;
+	float dx = 0.01f;
+	float dy = -0.01f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 s = transform.localScale;
+		if (s.x > 3 || s.x < 1) {
+
+
+			dx *= -1;
+
+
+		}
+		if (s.y > 3 || s.y < 0.1) {
+
+			dy *= -1;
+
+		}
+
+		s.x += dx;
+
+		s.y += dy;
+		transform.localScale = s;
+	
+
+		}
+}
